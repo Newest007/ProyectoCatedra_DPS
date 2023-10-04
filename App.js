852b3979128320be06b3106beee79be33ee77b57
Arian <adilsonarian@outlook.com>
2023-10-03 22:17:52 -0600

    Actualización

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, Button } from 'react-native';
-import Background from './components/registro';
+import Registro from './components/registro';
 import Login from './components/login';
 import Splash from './components/Splash';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useNavigation } from '@react-navigation/native';
+//import { useNavigation } from '@react-navigation/native';
 
 
 const Stack = createNativeStackNavigator();
@@ -27,6 +27,7 @@ export default function App() {
         <Stack.Screen name={'Splash'} component={Splash}  /*options={{headerShown: false}}*//>
         <Stack.Screen name="Login" component={Login} /*options={{headerShown: false}}*//>
         <Stack.Screen name="Home" component={HomeScreen} /*options={{headerShown: false}}*//>
+        <Stack.Screen name="Registro" component={Registro} /*options={{headerShown: false}}*//>
       </Stack.Navigator>
     </NavigationContainer>
   </>
