d0facc683c0f63d7b1839217a4092dbc87be21ea
Ander503 <andersonmelendez73@gmail.com>
2023-11-02 09:08:14 -0600

    Para modal

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Modal, StyleSheet, Text, View } from 'react-native';
 import Login from './components/Login';
 import Inicio from './components/Inicio';
 
@@ -9,101 +9,15 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
 
+import ModalExample from './components/Modal';
 
 export default function App() {
 
-  const Tab = createBottomTabNavigator();
-
-  
-
-  function HomeScreen() {
-    return (
-      <Inicio/>
-    );
-  }
-  
-  function SettingsScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
-
-  function Search() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Search</Text>
-      </View>
-    );
-  }
-
-  function Account() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Account</Text>
-      </View>
-    );
-  }
-
-  
-  function MyTabs() {
-    return (
-      <Tab.Navigator
-        screenOptions={{
-          tabBarStyle: { backgroundColor: '#2c278d', borderRadius: 5, },
-          tabBarActiveTintColor: '#ffffff',
-          tabBarInactiveTintColor: '#9f9ea7',
-        }}
-      >
-
-        
-          <Tab.Screen 
-            name="Inicio" 
-            component={HomeScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                                
-                  <FontAwesomeIcons name="home" color={color} size={size} />
-                
-              ),
-            }}
-          />
-
-
-       <Tab.Screen name="Buscar" component={Search} 
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <FontAwesomeIcons name="search" color={color} size={size} />
-            ),
-          }}
-        />
-
-        <Tab.Screen name="Ajustes" component={SettingsScreen} 
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <FontAwesomeIcons name="cog" color={color} size={size} />
-            ),
-          }}
-        />
-
-        <Tab.Screen name="Cuenta" component={Account} 
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <FontAwesomeIcons name="user-o" color={color} size={size} />
-            ),
-          }}
-        />
-       
-      </Tab.Navigator>
-    );
-  }
-
-  return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
  
+  return (
+
+    <ModalExample/>
+
   );
 }
 
@@ -115,3 +29,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+//*
+
+//
